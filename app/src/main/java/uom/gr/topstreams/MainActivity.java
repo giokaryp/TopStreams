6a1569b2ce422a144e726b3266851f2edf9615b0
giokaryp <it15238@uom.edu.gr>
2019-01-11 15:13:01 +0200

    login

--- app/src/main/java/uom/gr/topstreams/MainActivity.java
+++ app/src/main/java/uom/gr/topstreams/MainActivity.java
@@ -1,5 +1,6 @@
 package uom.gr.topstreams;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,17 +9,32 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     private static final String TAG = "GeoMainActivity";
 
 
     private DrawerLayout drawer;
+    private ImageView logo;
+    private TextView nameText;
 
 
     @Override
@@ -29,9 +45,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
+
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setupFirebaseAuth();
+
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     new StreamFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_topStreams);
         }
+
 
 
     }
@@ -69,10 +92,76 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new GameFragment()).commit();
                 break;
+//            case R.id.nav_myStreams:
+//                setTitle("My Streams");
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new MyStreamsFragment()).commit();
+            case R.id.nav_logout:
+                Log.d(TAG, "signOut: signing out");
+                FirebaseAuth.getInstance().signOut();
+//                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                break;
+            }
 
-        }
+
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
     }
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: started.");
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    updateUserInfo(user);
+
+
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    private void updateUserInfo(FirebaseUser user){
+
+        logo=findViewById(R.id.logoImageHeader);
+        nameText=findViewById(R.id.nameHeader);
+
+
+        Glide.with(getApplicationContext()).load(user.getPhotoUrl())
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(logo);
+        nameText.setText(user.getDisplayName());
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
 }
+
+
---
+++ app/src/main/java/uom/gr/topstreams/MyStreamsFragment.java
@@ -0,0 +1,39 @@
+package uom.gr.topstreams;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+
+public class MyStreamsFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_streams, container, false);
+    }
+
+
+
+}
